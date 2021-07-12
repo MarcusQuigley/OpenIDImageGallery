0dc3f4f6393a0bede040ef4a4a068fc34e3f1fe7
Marcus Quigley <marcusquigley@gmail.com>
2021-07-12 10:41:36 -0400

    Changing Identity.API port to avoid confusion

--- src/ImageGallery.Client/Startup.cs
+++ src/ImageGallery.Client/Startup.cs
@@ -28,7 +28,7 @@ namespace ImageGallery.Client
             // create an HttpClient used for accessing the API
             services.AddHttpClient("APIClient", client =>
             {
-                client.BaseAddress = new Uri("https://localhost:44366/");
+                client.BaseAddress = new Uri("https://localhost:44311/");
                 client.DefaultRequestHeaders.Clear();
                 client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
             });
