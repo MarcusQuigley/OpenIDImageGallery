4b81f132255708aee38a1fab9b3484ff5851f0f4
Marcus Quigley <marcusquigley@gmail.com>
2021-07-12 10:40:23 -0400

    Get claims from UserInfo endpoint

--- src/ImageGallery.Client/Startup.cs
+++ src/ImageGallery.Client/Startup.cs
@@ -50,7 +50,7 @@ namespace ImageGallery.Client
                     options.Scope.Add("profile");
                     options.SaveTokens = true;
                     options.ClientSecret = "secret";
-                    //options.GetClaimsFromUserInfoEndpoint = true;
+                     options.GetClaimsFromUserInfoEndpoint = true;
                 });
         }
 
